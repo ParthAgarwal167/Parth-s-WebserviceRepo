b65c8a0c0b5dfc92e59f9917668198c1b1c2a7e2
Parth Agarwal <parth.agarwal@espire.com>
2021-09-11 08:43:44 +0530

    Demo of Java code

---
+++ JAVA Demo/Demo/AreaOfSquare.java
@@ -0,0 +1,11 @@
+package com.demo;
+
+public class AreaOfSquare
+{
+	public static void main(String[] args)
+	{
+		float side = (float) 7.2;
+		float areaofsq = side * side;
+		System.out.println("Area of Square is: " + areaofsq);
+	}
+}

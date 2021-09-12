34b003146b9d5231a0b7bc3e12cb9c239c43b104
Parth Agarwal <51859086+ParthAgarwal167@users.noreply.github.com>
2021-09-13 01:41:50 +0530

    Demo of Java Code

---
+++ JAVA Demo/Demo/AreaOfRect.java
@@ -0,0 +1,11 @@
+package com.demo;
+
+public class AreaOfRect
+{
+	public static void main(String[] args)
+	{
+		float length = (float) 32.3, breadth = (float) 10.5;
+		float areaofrect = length * breadth;
+		System.out.println("Area of Rectangle is: " + areaofrect);
+	}
+}

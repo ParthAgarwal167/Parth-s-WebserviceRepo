5f8633618cd6cdd5dd4eb7b00962ac36a7900670
Parth Agarwal <51859086+ParthAgarwal167@users.noreply.github.com>
2021-10-19 09:26:03 +0530

    Add files via upload

---
+++ EcommerceAPIs/src/main/java/com/tut/EcommerceAPIs.java
@@ -0,0 +1,13 @@
+package com.tut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceAPIs {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceAPIs.class, args);
+	}
+
+}
